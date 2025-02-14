196cb43ab135e43a1a924becaa86e286864ae9c8
jordan <sudo.sendmail.jordan@gmail.com>
2025-02-13 23:02:36 -0800

    Refactor: Cards
    - refactor fetch
    - remove generateCard
    - fetch and map cards at build time
    - add fields to types

--- src/scripts/fetch.ts
+++ src/scripts/fetch.ts
@@ -4,51 +4,51 @@ import type { Icon } from "../scripts/types.ts";
 import type { Config } from "../scripts/types.ts";
 import type { List } from "../scripts/types.ts";
 
-export async function getDownloads(maintainer: string, repository: string): Promise<string> {
-  const response = await fetch(`https://api.github.com/repos/${maintainer}/${repository}/releases`);
-  const data = await response.json();
-  return data[0]["assets"][0]["download_count"];
+export async function getDownloads(maintainer: string, repository: string) {
+  try {
+    const response = await fetch(`https://api.github.com/repos/${maintainer}/${repository}/releases`);
+    const data = await response.json();
+    return data[0]["assets"][0]["download_count"];
+  } catch {
+    return "Unable to fetch download count";
+  }
 }
 
-export async function fetchThemes(storeResults: HTMLElement | null): Promise<HTMLElement | null> {
+export async function fetchThemes() {
   const response = await fetch("https://raw.githubusercontent.com/linux-themes/database/refs/heads/main/themes/index.yml");
   const data = await response.text();
-  const packages: List = jsyaml.load(data) as List;
-  if (storeResults) {
-    for (const index in packages["themes"]) {
-      storeResults.innerHTML += generateCardHTML(packages["themes"][index], index);
-    }
+  const yaml: List = jsyaml.load(data) as List;
+  let packages: Theme[] = [];
+  for (const index in yaml["themes"]) {
+    packages.push(yaml["themes"][index]);
   }
-  return storeResults;
+  return packages;
 }
 
-export async function fetchIcons(storeResults: HTMLElement | null): Promise<HTMLElement | null> {
+export async function fetchIcons() {
   const response = await fetch("https://raw.githubusercontent.com/linux-themes/database/refs/heads/main/icons/index.yml");
   const data = await response.text();
-  const packages: List = jsyaml.load(data) as List;
-  if (storeResults) {
-    for (const index in packages["icons"]) {
-      storeResults.innerHTML += generateCardHTML(packages["icons"][index], index);
-    }
+  const yaml: List = jsyaml.load(data) as List;
+  let packages: Icon[] = [];
+  for (const index in yaml["icons"]) {
+    packages.push(yaml["icons"][index]);
   }
-  return storeResults;
+  return packages;
 }
 
-export async function fetchConfigs(storeResults: HTMLElement | null): Promise<HTMLElement | null> {
+export async function fetchConfigs() {
   const response = await fetch("https://raw.githubusercontent.com/linux-themes/database/refs/heads/main/configs/index.yml");
   const data = await response.text();
-  const packages: List = jsyaml.load(data) as List;
-  if (storeResults) {
-    for (const index in packages["configs"]) {
-      storeResults.innerHTML += generateCardHTML(packages["configs"][index], index);
-    }
+  const yaml: List = jsyaml.load(data) as List;
+  let packages: Config[] = [];
+  for (const index in yaml["configs"]) {
+    packages.push(yaml["configs"][index]);
   }
-  return storeResults;
+  return packages;
 }
 
 export function getDesktopColor(desktop: string): string {
-  console.log(desktop);
-  switch (desktop.toLowerCase()) {
+  switch (desktop) {
     case "all":
       return "bg-green-300 text-green-900 dark:bg-green-600 dark:text-green-100";
     case "gnome":
@@ -61,7 +61,7 @@ export function getDesktopColor(desktop: string): string {
 }
 
 export function getCategoryColor(category: string): string {
-  switch (category.toLowerCase()) {
+  switch (category) {
     case "themes":
       return "bg-purple-300 text-purple-900 dark:bg-purple-600 dark:text-purple-100";
     case "icons":
@@ -71,30 +71,4 @@ export function getCategoryColor(category: string): string {
     default:
       return "bg-gray-200 text-gray-800";
   }
-}
-
-function generateCardHTML(data: Theme | Icon | Config, index: string): string {
-  if (data.category == "icons") data.desktop = "all";
-  return `
-      <div grade="${data.desktop.toLowerCase()}" category="${data.category.toLowerCase()}" class="card bg-white rounded-lg shadow-md justify-between flex flex-col relative top-0 transition-all hover:shadow-lg hover:-top-1 dark:bg-gray-800 dark:text-gray-200">
-        <div class="p-4 flex flex-col ">
-          <div class="flex gap-4 justify-between">
-            <div>
-            <h3 class="text-2xl font-bold mb-2">${data.name}</h3>
-            </div>
-            <h3 class="text-2xl font-bold mb-2">${index}</h3>
-            </div>
-            <img src="${data.url_icon}" alt="${data.name}" class="h-60 self-center">
-          <p class="text-gray-600 mb-4 text-xl dark:text-gray-300">${data.description}</p>
-          <div class="tags flex space-x-2 mb-4">
-            <span class="${getDesktopColor(data.desktop)} px-2 py-1 rounded-lg text-sm">${data.desktop}</span>
-            <span class="px-2 py-1 rounded-lg text-sm ${getCategoryColor(data.category)}">${data.category}</span>
-          </div>
-        </div>
-        <div class="bg-gray-100 rounded-b-lg text-center flex gap-2 justify-between dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600">
-          <a class="block font-medium text-lg size-full p-2 hover:bg-gray-500 rounded-bl-lg" href="/app#${data.category}_${index}">Details</a>
-          <a class="block font-medium text-lg size-full p-2 hover:bg-gray-500 rounded-br-lg" href="https://linuxthemes.org/download/">Install</a>
-        </div>
-      </div>
-  `;
 }
--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -1,28 +1,40 @@
 export type Theme = {
+  package_number: number;
   name: string;
   description: string;
   category: string;
   desktop: string;
+  maintainer: string;
+  repository: string;
+  url_git: string;
   url_icon: string;
   url_screenshot: string;
   url_package: string;
 };
 
 export type Icon = {
+  package_number: number;
   name: string;
   description: string;
   category: string;
   desktop: string;
+  maintainer: string;
+  repository: string;
+  url_git: string;
   url_icon: string;
   url_screenshot: string;
   url_package: string;
 };
 
 export type Config = {
+  package_number: number;
   name: string;
   description: string;
   category: string;
   desktop: string;
+  maintainer: string;
+  repository: string;
+  url_git: string;
   url_icon: string;
   url_screenshot: string;
   url_package: string;
